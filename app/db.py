e68d3e1964e149b501ce9c289082f409d41011f4
Aley133 <103179994+Aley133@users.noreply.github.com>
2025-09-09 01:18:53 +0500

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,1 +1,23 @@
+# app/db.py
+import os, psycopg
+from psycopg.rows import dict_row
+
+_DB_URL = os.getenv("DATABASE_URL")
+
+def get_conn():
+    return psycopg.connect(_DB_URL, row_factory=dict_row)
+
+def fetchrow(sql, args=()):
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute(sql, args)
+        return cur.fetchone()
+
+def fetchall(sql, args=()):
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute(sql, args)
+        return cur.fetchall()
+
+def execute(sql, args=()):
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute(sql, args)
 
